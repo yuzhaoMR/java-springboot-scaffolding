745a0beba85ba37bc520b72b33c0a696e304394f
yuzhaoMR <444894151@qq.com>
2020-08-29 16:20:43 +0800

    refactor code style and test ut

--- src/test/java/com/demo/scaffolding/JavaSpringbootScaffoldingApplicationTests.java
+++ src/test/java/com/demo/scaffolding/JavaSpringbootScaffoldingApplicationTests.java
@@ -3,11 +3,13 @@ package com.demo.scaffolding;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class JavaSpringbootScaffoldingApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+  @Test
+  void test() {
+    assertEquals(true, true);
+  }
 }
