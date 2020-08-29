b588c394c8c2e53ad799f91631aa14119235dc81
yuzhaoMR <444894151@qq.com>
2020-08-29 11:36:42 +0800

    initial p=repo

---
+++ src/main/java/com/demo/scaffolding/JavaSpringbootScaffoldingApplication.java
@@ -0,0 +1,13 @@
+package com.demo.scaffolding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringbootScaffoldingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringbootScaffoldingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/scaffolding/JavaSpringbootScaffoldingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.scaffolding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringbootScaffoldingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
