68bf12261ee233832536a6b0036f16c52e9d2643
yuzhaoMR <444894151@qq.com>
2020-08-29 16:07:18 +0800

    refactor code style

--- src/main/java/com/demo/scaffolding/JavaSpringbootScaffoldingApplication.java
+++ src/main/java/com/demo/scaffolding/JavaSpringbootScaffoldingApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JavaSpringbootScaffoldingApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(JavaSpringbootScaffoldingApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(JavaSpringbootScaffoldingApplication.class, args);
+  }
 }
